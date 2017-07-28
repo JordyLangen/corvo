6466c5d736f4a0763f4e250afaa41484b317a076
Jordy Langen <jordylangen@gmail.com>
2017-07-28 15:26:42 +0200

    Formatting

--- corvo-example/src/main/java/jordylangen/corvo/example/MealsView.kt
+++ corvo-example/src/main/java/jordylangen/corvo/example/MealsView.kt
@@ -6,6 +6,4 @@ import android.widget.FrameLayout
 import com.jordylangen.corvo.annotations.BindsTo
 
 @BindsTo(dependency = MealsPresenter::class, module = SampleModule::class)
-class MealsView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr)
+class MealsView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(context, attrs, defStyleAttr)
