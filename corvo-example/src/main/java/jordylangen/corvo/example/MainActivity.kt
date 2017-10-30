49794ba4570ede47a1b7cbce8e34ecedc1be1162
JordyLangen <jordylangen@gmail.com>
2017-10-30 12:53:39 +0100

    Updated the build versions

--- corvo-example/src/main/java/jordylangen/corvo/example/MainActivity.kt
+++ corvo-example/src/main/java/jordylangen/corvo/example/MainActivity.kt
@@ -3,8 +3,6 @@ package jordylangen.corvo.example
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.jordylangen.corvo.Corvo
-import com.jordylangen.corvo.CorvoBindingDependencyResolver
-import com.jordylangen.corvo.DaggerCorvoComponent
 import com.squareup.coordinators.Coordinators
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val component = DaggerCorvoComponent.builder()
+        val component = com.jordylangen.corvo.DaggerCorvoComponent.builder()
                 .sampleModule(SampleModule())
                 .build()
 
-        val resolver = CorvoBindingDependencyResolver(component)
+        val resolver = com.jordylangen.corvo.CorvoBindingDependencyResolver(component)
 
         val corvo = Corvo(resolver)
 
